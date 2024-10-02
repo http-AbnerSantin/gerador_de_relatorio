38a5f45f16a191b710264b00ef313861048f6df9
http-AbnerSantin <abner.santin11@gmail.com>
2024-10-02 11:27:10 -0300

    alterações

--- gerar_pdf.php
+++ gerar_pdf.php
@@ -7,66 +7,90 @@ $dataInicial = $_POST["dataInicial"];
 $dataFinal = $_POST["dataFinal"];
 
 //WHERE `data` BETWEEN '$dataInicial' AND '$dataFinal'
-$sql = "SELECT * FROM relatorio WHERE `data` BETWEEN '$dataInicial' AND '$dataFinal'";
+$sql = "SELECT * FROM relatorio2 WHERE `data` BETWEEN '$dataInicial' AND '$dataFinal'";
+// $sql = "SELECT * FROM relatorio2 WHERE `data` BETWEEN '2020-09-30' AND '2020-10-01'";
+
 $res = $conn->query($sql);
 
 //horario da emissão do relatório
 $hora = date('H:i:s');
+$empresa = "CANINANA & RIBEIRO LTDA";
 
+$html="";
 
 //condição caso o banco vanh vazio
 if($res->num_rows > 0) {
 
-  $html = "<table style='margin: 0 auto;'>";
-  $html .= "<style>
-        body { font-family: Arial, sans-serif; font-size: 12px; color: #333; }
-        .container { width: 100%; margin: 0 auto; }
-        .header, .footer { text-align: center; margin-bottom: 20px; }
-        .header { font-size: 14px; font-weight: bold; border-bottom: 1px solid #000; }
-        .footer { font-size: 10px; margin-top: 40px; border-top: 1px solid #000; padding-top: 10px; }
-        .content { margin-top: 20px; }
-        .content h2 { font-size: 16px; text-align: center; margin-bottom: 20px; }
-        table { width: 100%; border-collapse: collapse; margin-bottom: 20px; }
-        th, td { padding: 2px; border: 1px solid #ddd; text-align: left; }
-        th { background-color: #f2f2f2; font-weight: bold; }
-        .error { color: red; font-weight: bold; }
-    </style>";
+  $html = "<style>
+  body { font-family: Arial, sans-serif; font-size: 12px; color: #333; }
+  .container { width: 100%; margin: 0 auto; }
+  .header, .footer { text-align: center; margin-bottom: 20px; }
+  .header { font-size: 14px; font-weight: bold; border-bottom: 1px solid #000; }
+  .footer { font-size: 10px; margin-top: 40px; border-top: 1px solid #000; padding-top: 10px; }
+  .content { margin-top: 20px; }
+  .content h2 { font-size: 16px; text-align: center; margin-bottom: 20px; }
+  table { width: 100%; border-collapse: collapse; margin-bottom: 20px; }
+  th, td { padding: 2px; border: 1px solid #ddd; text-align: left; }
+  th { background-color: #f2f2f2; font-weight: bold; }
+  .error { color: red; font-weight: bold; }
+  </style>";
+  //cabeçalho
+  $html .= "<h3>$empresa</h3>";
   $html .= "<p>Vendas por Funcionario no periodo de $dataInicial ate $dataFinal</p>";
   $html .= "<p>Hora... $hora</p>";
-
-
+  
+  
+  $html .= "<table style='margin: 0 auto;'>";
   //vendedor
-  $sqlVendedor = "SELECT vendedor FROM relatorio GROUP BY vendedor";
-
+  $sqlVendedor = "SELECT FUNCIONARIO FROM relatorio2 GROUP BY FUNCIONARIO";
+  
   $sqlVendedorRes = $conn->query($sqlVendedor);
-  $linha;
+  
   while($linha = $sqlVendedorRes->fetch_array()) {
-    $vendedor = $linha['vendedor'];
-    $html .= '<p>Vendedor: '.$linha['vendedor']. '</p>';
+    $vendedor = $linha['FUNCIONARIO'];
+    $html .= '<p>Vendedor: '.$vendedor. '</p>';
     $html .= "<tr>";
     $html .= "<th> DATA </th>";
     $html .= "<th> PEDIDO </th>";
     $html .= "<th> VALOR </th>";
+    $html .= "<th> PERDE/GANHA </th>";
     $html .= "</tr>";
 
-    $sqlVendas = "SELECT * FROM relatorio WHERE vendedor = '$vendedor'";
-
+    $sqlVendas = "SELECT * FROM relatorio2 WHERE FUNCIONARIO = '$vendedor' AND `data` BETWEEN '$dataInicial' AND '$dataFinal'";
+    // $sqlVendas = "SELECT * FROM relatorio2 WHERE FUNCIONARIO = '$vendedor' AND `data` BETWEEN '2020-09-30' AND '2020-10-01'";
     $retorno = $conn->query($sqlVendas);
 
+  
     while($row2 = $retorno->fetch_array()) {
       $html .= "<tr>";
-      $html .= "<td>".$row2['data']."</td>";
-      $html .= "<td>".$row2['pedido']."</td>";
-      $html .= "<td>".$row2['valor']."</td>";
+      $html .= "<td>".$row2['DATA']."</td>";
+      $html .= "<td>".$row2['NUMERO']."</td>";
+      $html .= "<td>".$row2['TOTAL']."</td>";
+      $html .= "<td>".$row2['PERDE_GANHA']."</td>";
       $html .= "</tr>";
     }
   }
+  $sqlSomaTotal = "SELECT SUM(TOTAL) FROM relatorio2 WHERE `DATA` BETWEEN '$dataInicial' AND '$dataFinal'";
+  // $sqlSomaTotal = "SELECT SUM(TOTAL) FROM relatorio2 WHERE `DATA` BETWEEN '2020-09-30' AND '2020-10-01'";
+  $sqlSomaTotalRes = $conn->query($sqlSomaTotal);
 
+  $row3 = $sqlSomaTotalRes->fetch_array();
+    $html .= "<tr>";
+    $html .= "<th> Total Geral </th>";
+    $html .= "</tr>";
+    $html .= "<tr>";
+    $html .="<td>R$".$row3[0]."</td>";
+    $html .="</tr>";
+  
   $html .= "</table>";
 
 } else {
   $html .= "Nenhum dado encontrado";
 }
+
+
+
+
 // echo $html;
 use Dompdf\Dompdf;
 
@@ -80,4 +104,4 @@ $dompdf->setPaper('A4', 'portrait');
 
 $dompdf->render();
 
-$dompdf->stream();
+$dompdf->stream('relatorio.pdf', array("Attachment" => false));
